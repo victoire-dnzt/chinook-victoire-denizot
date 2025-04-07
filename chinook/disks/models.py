015a5994b56a589acecb0e3f7765a82dc3e7dbfb
victoire-dnzt <victoire.denizot@student.isae-supaero.fr>
2025-04-07 16:46:12 +0200

    liste_albums_done

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -3,26 +3,24 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class Artist (models.Models):
+class Artist (models.Model):
     name = models.CharField(max_length=255)
     
 def __str__(self):
         return self.name
 
-class Album (models.Models):
-    name = models.CharField(max_length=255)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)  # Un album appartient à un artiste
-
+class Album (models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist,on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
-class Track (models.Models):
+class Track (models.Model):
     name = models.CharField(max_length=255)
-    composer = models.CharField(max_length=255)  # compositeur de la chanson
-    milliseconds = models.IntegerField()  # durée en millisecondes
-    bytes = models.IntegerField()  # taille du fichier en octets
-    unitPrice = models.DecimalField(max_digits=10, decimal_places=2)  # prix de la piste
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)  # une piste appartient à un album
-
+    composer = models.CharField(max_length=255, null=True)
+    milliseconds = models.IntegerField()
+    bytes = models.IntegerField()
+    unitPrice = models.DecimalField(decimal_places=1,max_digits=3)
+    album = models.ForeignKey(Album,on_delete=models.CASCADE)
     def __str__(self):
         return self.name
